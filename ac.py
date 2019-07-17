3256f09d7ef5c59fa4dfa846c0ce71b16aad2ee1
chandana341 <52447525+chandana341@users.noreply.github.com>
2019-07-16 20:41:25 -0700

    Update ac.py

--- ac.py
+++ ac.py
@@ -1,11 +1,13 @@
 m1=int(input())
-arun=list(map(int,input().split()))
+a=list(map(int,input().split()))
 ln=[]
-for i in arun:
-    if arun.count(i)>1:
+for i in a:
+    if a.count(i)>1:
         if str(i) not in ln:
             ln.append(str(i))
 if len(ln)==0:
     print("unique")
 else:
     print(" ".join(ln))
+
+    
