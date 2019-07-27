e188496e9aa567e159e35a9254b3599c5f6588f9
chandana341 <52447525+chandana341@users.noreply.github.com>
2019-07-27 13:27:34 +0530

    Update h131.py

--- h131.py
+++ h131.py
@@ -1,10 +1,10 @@
-inte=int(input())
-a=list(map(int,input().split()))
-d=sorted(a)
-k=d[::-1]
-p=[]
+inter=int(input())
+a1=list(map(int,input().split()))
+d=sorted(a1)
+k1=d[::-1]
+p1=[]
 for i in range(0,len(a)):
-    p.append(k[i])
-    p.append(d[i])
+    p1.append(k1[i])
+    p1.append(d[i])
 for j in p[:len(a)]:
     print(j,end=" ")
